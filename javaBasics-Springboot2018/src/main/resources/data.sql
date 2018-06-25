e0fe38f59375089d8cb20da4a646067c9be9eca7
AdamClmns <adam@adamclmns.com>
2018-06-25 06:15:25 -0500

    Creating default data script

---
+++ javaBasics-Springboot2018/src/main/resources/data.sql
@@ -0,0 +1,25 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  Adam
+ * Created: Jun 24, 2018
+ */
+
+INSERT INTO BOOK (ID, TITLE, AUTHOR, PUBLISHED, LAST_BORROWED, LAST_RETURNED )
+VALUES(CAST('fd6894ce08bb4488a94960a38c890a2a' AS BINARY), 'Gone with the Wind', 'Margaret Mitchell',TO_DATE('1939-01-01', 'YYYY-MM-DD'),TO_DATE('2018-01-01','YYYY-MM-DD'),TO_DATE('2018-01-09','YYYY-MM-DD'));
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  Adam
+ * Created: Jun 24, 2018
+ */
+
+INSERT INTO BOOK (ID, TITLE, AUTHOR, PUBLISHED, LAST_BORROWED, LAST_RETURNED )
+VALUES(CAST('9d7c46e96c844ef8b0260ced2b5d5a71' AS BINARY), 'A Christmas Carol', 'Charlse Dickens',TO_DATE('1843-01-01', 'YYYY-MM-DD'),TO_DATE('2018-01-01','YYYY-MM-DD'),TO_DATE('2018-01-09','YYYY-MM-DD'));  
